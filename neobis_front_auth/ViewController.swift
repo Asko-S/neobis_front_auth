2d7cda55df411b7c7217cd7876791df96dc93c8e
soronbekov-a-auca-2021 <soronbekov_a@auca.kg>
2023-09-25 11:41:05 +0600

    Initial Commit

---
+++ neobis_front_auth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  neobis_front_auth
+//
+//  Created by Askar Soronbekov on 25/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
